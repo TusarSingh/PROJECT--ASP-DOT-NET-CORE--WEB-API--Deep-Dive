61de41741091e8973948a0fb48aee8be1be3a325
Tusar Singh <rts0941@gmail.com>
2024-08-08 19:27:03 +0530

    Create a Implementation ' AccountByIdentityInEFCRepo & UserRegistrationByIdentityInEFC ' for Account Logics By Identity In EFC in ' Radhey.Repo '

---
+++ Radhey/Radhey.Repository/Implementation/IdentityByEFC__Repo/AccountByIdentityInEFCRepo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Radhey.Model;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+
+using Radhey.DAL;
+using Radhey.DAL.IdentityTables;
+using Radhey.Repository.Interface.IIdentityByEFC__Repo;
+using Radhey.Repository.Interface.IIdentityByEFC__Repo.IUserRegistration;
+
+
+
+
+
+namespace Radhey.Repository.Implementation.IdentityByEFC__Repo
+{
+    public class AccountByIdentityInEFCRepo : IAccountByIdentityInEFCRepo
+    {
+
+        private readonly IUserRegistrationByIdentityInEFC _userRegistrationByIdentityInEFC;
+
+
+        public AccountByIdentityInEFCRepo(
+                                            IUserRegistrationByIdentityInEFC userRegistrationByIdentityInEFC
+
+                                            )
+        {
+            this._userRegistrationByIdentityInEFC = userRegistrationByIdentityInEFC;
+        }
+
+
+
+
+        public async Task<ResponseComModel> UserRegistration(UserRegistration__ReqModel userRegistration)
+        {
+
+            ResponseComModel response  = new ResponseComModel();
+
+            var user = await _userRegistrationByIdentityInEFC.UserRegistration(userRegistration);
+
+            response.StatusCode = user.StatusCode;
+
+            return response;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Radhey/Radhey.Repository/Implementation/IdentityByEFC__Repo/UserRegistration/UserRegistrationByIdentityInEFC.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Radhey.Repository.Interface.IIdentityByEFC__Repo;
+using Radhey.Repository.Interface.IIdentityByEFC__Repo.IUserRegistration;
+
+using Radhey.Model;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+using Radhey.DAL.IdentityTables;
+
+using Radhey.ORM.IdentityByEFC;
+using Radhey.ORM.IdentityByEFC.Interface;
+using Radhey.ORM.IdentityByEFC.Implementation;
+
+
+namespace Radhey.Repository.Implementation.IdentityByEFC__Repo.UserRegistration
+{
+    public class UserRegistrationByIdentityInEFC : IUserRegistrationByIdentityInEFC
+    {
+
+        private readonly ICustomUserManager _customUserManager;
+        
+
+
+        public UserRegistrationByIdentityInEFC(
+                                                ICustomUserManager customUserManager
+                
+                                                )
+        {
+            this._customUserManager = customUserManager;
+            
+        }
+
+
+        #region User Registration with out Password Encryption
+
+        //public async Task<ResponseComModel> UserRegistration(UserRegistration__ReqModel userRegistration__ReqModel)
+        //{
+
+        //    ResponseComModel response = new ResponseComModel();
+
+        //    TblApplicationUser tblApplicationUser = new TblApplicationUser()
+        //    {
+        //        FirstName = userRegistration__ReqModel.FirstName,
+        //        Email = userRegistration__ReqModel.Email,
+        //        PasswordHash = userRegistration__ReqModel.Password,
+        //        PhoneNumber = userRegistration__ReqModel.Phone
+        //    };
+
+        //    var user = await this._customUserManager.PostCreateAsync(tblApplicationUser);
+
+        //    if (user.Succeeded)
+        //    {
+        //        response.StatusCode = 200;
+        //    }
+        //    else
+        //    {
+        //        response.StatusCode = 400;
+        //    }
+
+
+
+
+        //    return response;
+        //}
+
+        #endregion
+
+        #region User Registration with Password Encryption
+
+        public async Task<ResponseComModel> UserRegistration(UserRegistration__ReqModel userRegistration__ReqModel)
+        {
+
+            ResponseComModel response = new ResponseComModel();
+
+            TblApplicationUser tblApplicationUser = new TblApplicationUser()
+            {
+                FirstName = userRegistration__ReqModel.FirstName,
+                Email = userRegistration__ReqModel.Email,
+                PasswordHash = userRegistration__ReqModel.Password,
+                PhoneNumber = userRegistration__ReqModel.Phone
+            };
+
+            var user = await this._customUserManager.PostCreateAsync(tblApplicationUser, tblApplicationUser.PasswordHash);
+
+            if (user.Succeeded)
+            {
+                response.StatusCode = 200;
+            }
+            else
+            {
+                response.StatusCode = 400;
+            }
+
+
+
+
+            return response;
+        }
+
+        #endregion
+
+
+
+    }
+}
