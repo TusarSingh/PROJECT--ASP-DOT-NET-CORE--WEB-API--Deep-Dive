f2ed41255d374103332a5ad607a428cf57438668
Tusar Singh <rts0941@gmail.com>
2024-08-08 19:30:47 +0530

    Create a CommonModel ' ResponseComModel ' for Api Status In Whole Project in ' Radhey.Radhey.Model '

---
+++ Radhey/Radhey.Model/CommonModel/ResponseComModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radhey.Model.CommonModel
+{
+    public class ResponseComModel
+    {
+
+#nullable disable
+        public int StatusCode { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string StatusMessage { get; set; }
+
+
+    }
+
+    public class ResponseComModel<T> : ResponseComModel
+    {
+        public T Data { get; set; }
+
+        public object ValidationError { get; set; }
+    }
+}
