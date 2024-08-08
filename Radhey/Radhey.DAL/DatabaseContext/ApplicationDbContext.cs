c4df1555a45ae179dbce8a635f2abc1a889923a6
Tusar Singh <rts0941@gmail.com>
2024-08-08 19:46:29 +0530

    Create a DatbaseContext  ' ApplicationDbContext ' for Application Tables  In Whole Project in ' Radhey.DAL '

---
+++ Radhey/Radhey.DAL/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Radhey.DAL.IdentityTables;
+
+namespace Radhey.DAL.DatabaseContext
+{
+    public class ApplicationDbContext : IdentityDbContext<TblApplicationUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) : base( options )
+        {
+            
+        }
+
+
+
+
+        public DbSet<TblApplicationUser> AspNetUser { get; set; }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+
+
+
+    }
+}
