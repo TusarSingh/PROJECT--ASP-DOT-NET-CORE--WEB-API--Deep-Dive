9656962139df939a9f110c7ee329fe4d9a0eebf4
Tusar Singh <rts0941@gmail.com>
2024-08-08 19:24:04 +0530

    Create a Interface ' IAccountByIdentityInEFCRepo & IUserRegistrationByIdentityInEFC ' for Account Logics By Identity In EFC in ' Radhey.Repo '

---
+++ Radhey/Radhey.Repository/Interface/IIdentityByEFC__Repo/IAccountByIdentityInEFCRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Radhey.Model;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+
+
+
+namespace Radhey.Repository.Interface.IIdentityByEFC__Repo
+{
+    public interface IAccountByIdentityInEFCRepo
+    {
+
+        public Task<ResponseComModel> UserRegistration(UserRegistration__ReqModel userRegistration__Req);
+
+
+    }
+}
---
+++ Radhey/Radhey.Repository/Interface/IIdentityByEFC__Repo/IUserRegistration/IUserRegistrationByIdentityInEFC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Radhey.Model;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+
+namespace Radhey.Repository.Interface.IIdentityByEFC__Repo.IUserRegistration
+{
+    public interface IUserRegistrationByIdentityInEFC
+    {
+
+        public Task<ResponseComModel> UserRegistration(UserRegistration__ReqModel userRegistration__ReqModel);
+
+
+
+
+
+
+
+
+    }
+}
