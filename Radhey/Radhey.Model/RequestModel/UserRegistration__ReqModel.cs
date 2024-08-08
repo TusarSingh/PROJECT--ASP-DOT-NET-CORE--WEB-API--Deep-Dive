62ff90f3647681876b5a8821627a218c00efbee1
Tusar Singh <rts0941@gmail.com>
2024-08-08 19:33:41 +0530

    Create a RequestModel ' UserRegistration__ReqModel ' for User Registration In Whole Project in ' Radhey.Model '

---
+++ Radhey/Radhey.Model/RequestModel/UserRegistration__ReqModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radhey.Model.RequestModel
+{
+    public class UserRegistration__ReqModel
+    {
+#nullable disable
+        public string FirstName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }    
+
+        public string Phone { set; get; }
+    }
+}
