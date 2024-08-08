526e1ed3670f84ac848ca5157e6370445292c105
Tusar Singh <rts0941@gmail.com>
2024-08-08 19:20:39 +0530

    Create a Implementation ' AccountByIdentityInEFCBAL ' for Account Logics By Identity In EFC in ' Radhey.BAL '

---
+++ Radhey/Radhey.BAL/Implementation/IdentityByEFCBAL/AccountByIdentityInEFCBAL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Radhey.BAL;
+using Radhey.BAL.Interface;
+using Radhey.BAL.Interface.IIdentityByEFCBAL;
+
+using Radhey.Model;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+
+using Radhey.Repository;
+using Radhey.Repository.Interface;
+using Radhey.Repository.Interface.IIdentityByEFC__Repo;
+
+
+
+
+namespace Radhey.BAL.Implementation.IdentityByEFCBAL
+{
+    public class AccountByIdentityInEFCBAL : IAccountByIdentityInEFCBAL
+    {
+
+        private readonly IAccountByIdentityInEFCRepo _accountByIdentityInEFCRepo;
+
+
+
+
+        public AccountByIdentityInEFCBAL(
+                                            IAccountByIdentityInEFCRepo accountByIdentityInEFCRepo
+                                        )
+        {
+                this._accountByIdentityInEFCRepo = accountByIdentityInEFCRepo;
+        }
+
+        public async Task<ResponseComModel> UserRegistration(UserRegistration__ReqModel userRegistration__ReqModel)
+        {
+
+            ResponseComModel response = new ResponseComModel();
+
+            var result = await _accountByIdentityInEFCRepo.UserRegistration(userRegistration__ReqModel);    
+
+            response.StatusCode = result.StatusCode;
+
+            return response;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
