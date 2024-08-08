23179bb50ffdb1547a77ccb141b13122db4e3157
Tusar Singh <rts0941@gmail.com>
2024-08-08 19:17:15 +0530

    Create a Interface ' IAccountByIdentityInEFCBAL ' for Account Logics By Identity In EFC in ' Radhey.BAL '

---
+++ Radhey/Radhey.BAL/Interface/IIdentityByEFCBAL/IAccountByIdentityInEFCBAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Radhey.Model;
+using Radhey.Model.CommonModel;
+using Radhey.Model.RequestModel;
+
+
+namespace Radhey.BAL.Interface.IIdentityByEFCBAL
+{
+    public interface IAccountByIdentityInEFCBAL
+    {
+
+        public Task<ResponseComModel> UserRegistration(UserRegistration__ReqModel userRegistration__ReqModel);
+    }
+}
