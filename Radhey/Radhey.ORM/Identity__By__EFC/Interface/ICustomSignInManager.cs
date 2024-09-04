fdea618b3b87a2f64b1b2adcecb72592a6902f44
Tusar Singh <rts0941@gmail.com>
2024-09-04 17:37:34 +0530

    Create a ' Account Related ' Some Files & Folder for Identity Approach

---
+++ Radhey/Radhey.ORM/Identity__By__EFC/Interface/ICustomSignInManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radhey.ORM.Identity__By__EFC.Interface
+{
+    public interface ICustomSignInManager
+    {
+    }
+}
